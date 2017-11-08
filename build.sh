35b2d74668b045b3f189160cdda84ff851d68964
suraj mittal <suraj.mittal@hashworks.co>
2017-11-08 13:06:26 +0530

    Changed to python 3.6

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ yum install -y \
     gcc \
     gcc-c++ \
     lapack-devel \
-    python27-devel \
-    python27-virtualenv \
+    python36-devel \
+    python36-virtualenv \
     findutils \
     zip
 
@@ -18,15 +18,16 @@ do_pip () {
     pip install --upgrade pip wheel
     pip install --use-wheel --no-binary numpy numpy
     pip install --use-wheel --no-binary scipy scipy
-    pip install --use-wheel sklearn
+    pip install --use-wheel smart-open
+    pip install --use-wheel gensim
 }
 
 strip_virtualenv () {
     echo "venv original size $(du -sh $VIRTUAL_ENV | cut -f1)"
-    find $VIRTUAL_ENV/lib64/python2.7/site-packages/ -name "*.so" | xargs strip
+    find $VIRTUAL_ENV/lib64/python3.6/site-packages/ -name "*.so" | xargs strip
     echo "venv stripped size $(du -sh $VIRTUAL_ENV | cut -f1)"
 
-    pushd $VIRTUAL_ENV/lib64/python2.7/site-packages/ && zip -r -9 -q /outputs/venv.zip * ; popd
+    pushd $VIRTUAL_ENV/lib64/python3.6/site-packages/ && zip -r -9 -q /outputs/venv.zip * ; popd
     echo "site-packages compressed size $(du -sh /outputs/venv.zip | cut -f1)"
 
     pushd $VIRTUAL_ENV && zip -r -q /outputs/full-venv.zip * ; popd
@@ -34,16 +35,16 @@ strip_virtualenv () {
 }
 
 shared_libs () {
-    libdir="$VIRTUAL_ENV/lib64/python2.7/site-packages/lib/"
-    mkdir -p $VIRTUAL_ENV/lib64/python2.7/site-packages/lib || true
+    libdir="$VIRTUAL_ENV/lib64/python3.6/site-packages/lib/"
+    mkdir -p $VIRTUAL_ENV/lib64/python3.6/site-packages/lib || true
     cp /usr/lib64/atlas/* $libdir
     cp /usr/lib64/libquadmath.so.0 $libdir
     cp /usr/lib64/libgfortran.so.3 $libdir
 }
 
 main () {
-    /usr/bin/virtualenv \
-        --python /usr/bin/python /sklearn_build \
+    /usr/bin/virtualenv-3.6 \
+        --python /usr/bin/python3.6 /sklearn_build \
         --always-copy \
         --no-site-packages
     source /sklearn_build/bin/activate
